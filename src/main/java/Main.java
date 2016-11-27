4c8395c2bd79fed6480ad6b9bb02e5ea3343de55
Lamar, Frank <frankie_lamar1@homedepot.com>
2016-11-27 08:39:26 -0500

    add handlebar dependency

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,13 @@
 /**
  * Created by fl76at on 11/25/16.
  */
+
+import static spark.Spark.get;
 public class Main {
     public static void main(String[] args){
+        get("/hello", (req, res) -> "Hello World");
 
+        get("/", (req, res) -> "Welcome Students");
 
     }
 }
