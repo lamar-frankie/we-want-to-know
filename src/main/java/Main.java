a1805c475152967b29a611a6b04d5f37bfa9c5fa
Lamar, Frank <frankie_lamar1@homedepot.com>
2016-11-28 05:57:53 -0500

    added get request and sign-in screen

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,12 +2,23 @@
  * Created by fl76at on 11/25/16.
  */
 
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+import static java.lang.System.in;
 import static spark.Spark.get;
+import static spark.Spark.post;
+
 public class Main {
     public static void main(String[] args){
-        get("/hello", (req, res) -> "Hello World");
 
-        get("/", (req, res) -> "Welcome Students");
+        get("/", (req, res) -> {
+            return new ModelAndView(null, "index.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        post("/sign-in", (req, res) -> {
+            return new ModelAndView(null, "sign-in.hbs");
+        }, new HandlebarsTemplateEngine());
 
     }
 }
